1db36da8e7f1396e534788b8dbe48d475949edae
Enrique M. Talavera <kiquetal@kathy.site>
2015-01-03 23:20:10 -0300

    Day 1

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -5,23 +5,22 @@
  ( if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (>= x 0) x (* x -1)))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= 0 (mod n divisor)) true false ))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [x] (cond (= 0 (mod x 15)) "gotcha!" (= 0 (mod x 5)) "buzz" (= 0 (mod x 3)) "fizz" :else ""))
+
 
 (defn teen? [age] (if (>= age 13) (if (<= age 19) true false) false))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [x] (not (teen? x)))
 
-(defn generic-doublificate [x]
-  ":(")
 
-(defn leap-year? [year]
-  ":(")
+(defn generic-doublificate [x] (cond (number? x) (* 2 x) (empty? x) nil (or (list? x) (vector? x)) (* 2 (count x)) :else true))
+
+(defn leap-year? [x] (or(and (= 0 (mod x 4)) (not(= 0 (mod x 100)))) (= 0 (mod x 400))))
+
 
 ; '_______'
